a7917df950920d49114bc2e0dada7e82e55e726e
Kelly Franco <kellycjfranco@yahoo.com.br>
2022-08-24 16:17:02 -0300

    variaveis

---
+++ aprenda-a-programar/tarefa-dia-2/pratica-1.js
@@ -0,0 +1,26 @@
+// Preparando uma mensagem de uma agência de turismo
+// Abra o arquivo pratica-1.js com o VSCODE ou o editor de código de sua preferência.
+// Agora no arquivo pratica-1.js, declare 5 variáveis que vão armazenar os seguintes valores:
+// seu nome
+// seu email
+// sua profissão
+// sua idade
+// o lugar que você sonha um dia conhecer
+
+let nomeVariavel 
+nomeVariavel = "Kelly"
+let emailVariavel 
+emailVariavel = "kellycjfranco@yahoo.com.br"
+let profissaoVariavel
+profissaoVariavel = "Secretária"
+let idadeVariavel
+idadeVariavel = "33 anos"
+let lugarVariavel
+lugarVariavel = "Montreal"
+
+console.log(nomeVariavel)
+console.log(emailVariavel)
+console.log(profissaoVariavel)
+console.log(idadeVariavel)
+console.log(lugarVariavel)
+
